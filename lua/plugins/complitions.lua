ae35218e68a24645510f73e68cf9f159cf5d498b
Fedoruk Eduard <eduard.fedoruk@nike.com>
2024-03-21 14:51:41 +0200

    add complition shortcut

--- lua/plugins/complitions.lua
+++ lua/plugins/complitions.lua
@@ -20,6 +20,7 @@ return {
 						require("luasnip").lsp_expand(args.body)
 					end,
 				},
+				completion = { completeopt = 'menu,menuone,noinsert' },
 				window = {
 					completion = cmp.config.window.bordered(),
 					documentation = cmp.config.window.bordered(),
@@ -27,7 +28,7 @@ return {
 				mapping = cmp.mapping.preset.insert({
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-o>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
 					["<CR>"] = cmp.mapping.confirm({ select = true }),
 					["<C-k>"] = cmp.mapping.select_prev_item(),
