de386e440473b952fb4f88d4daa665ddc469ce1b
Fedoruk Eduard <eduard.fedoruk@nike.com>
2024-03-25 18:10:04 +0200

    change neo tree settings

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,49 +1,65 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-		{
-			's1n7ax/nvim-window-picker',
-			version = '2.*',
-			config = function()
-				require 'window-picker'.setup({
-					filter_rules = {
-						include_current_win = false,
-						autoselect_one = true,
-						-- filter using buffer options
-						bo = {
-							-- if the file type is one of following, the window will be ignored
-							filetype = { 'neo-tree', "neo-tree-popup", "notify" },
-							-- if the buffer type is one of following, the window will be ignored
-							buftype = { 'terminal', "quickfix" },
-						},
-					},
-				})
-			end,
-		},
-	},
-	config = function()
-		require('neo-tree').setup({
-			close_if_last_window = true,
-			window = {
-				position = 'right'
-			},
-			filesystem = {
-				filtered_items = {
-					visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
-					hide_dotfiles = false,
-					hide_gitignored = false,
-					hide_by_name = {
-						'.git',
-						'.DS_Store',
-					},
-				},
-			}
-		})
-		vim.keymap.set('n', '<Leader>n', ':Neotree filesystem toggle right<CR>', {})
-	end
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
+        "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+        {
+            's1n7ax/nvim-window-picker',
+            version = '2.*',
+            config = function()
+                require 'window-picker'.setup({
+                    filter_rules = {
+                        include_current_win = false,
+                        autoselect_one = true,
+                        -- filter using buffer options
+                        bo = {
+                            -- if the file type is one of following, the window will be ignored
+                            filetype = { 'neo-tree', "neo-tree-popup", "notify" },
+                            -- if the buffer type is one of following, the window will be ignored
+                            buftype = { 'terminal', "quickfix" },
+                        },
+                    },
+                })
+            end,
+        },
+    },
+    config = function()
+        require('neo-tree').setup({
+            close_if_last_window = true,
+            enable_diagnostics = true,
+            git_status = {
+                symbols = {
+                    -- Change type
+                    added     = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
+                    modified  = "", -- or "", but this is redundant info if you use git_status_colors on the name
+                    deleted   = "✖", -- this can only be used in the git_status source
+                    renamed   = "󰁕", -- this can only be used in the git_status source
+                    -- Status type
+                    untracked = "",
+                    ignored   = "",
+                    unstaged  = "",
+                    staged    = "",
+                    conflict  = "",
+                }
+            },
+            window = {
+                position = 'right'
+            },
+            filesystem = {
+                filtered_items = {
+                    visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+                    hide_dotfiles = false,
+                    hide_gitignored = false,
+                    hide_by_name = {
+                        '.git',
+                        '.DS_Store',
+                    },
+                },
+            }
+        })
+        vim.keymap.set('n', '<Leader>n', ':Neotree filesystem toggle right<CR>', {})
+    end
 }
