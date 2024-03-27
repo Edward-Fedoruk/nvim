a86e1949137626830d53d08423f65880a7a6c8cb
Fedoruk Eduard <eduard.fedoruk@nike.com>
2024-03-27 19:45:00 +0200

    resize windows with arrow keys

--- lua/remap.lua
+++ lua/remap.lua
@@ -14,6 +14,7 @@ vim.g.mapleader = ' '
 vim.g.loaded_netrwPlugin = 1
 vim.g.loaded_netrw = 1
 
+local options = { noremap = true, silent = true }
 vim.keymap.set('n', '<leader>ex', vim.cmd.Ex)
 vim.keymap.set('n', '<leader>ln', function()
     vim.opt.relativenumber = not vim.opt.relativenumber:get()
@@ -21,11 +22,16 @@ end, { noremap = true, silent = true, desc = "Toggle Relative Numbers" })
 vim.keymap.set('n', '<Leader>/', ':nohlsearch<CR>', {})
 vim.keymap.set('t', 'jk', '<C-\\><C-n>', { noremap = true })
 vim.keymap.set('n', '<Leader>s', ':w<CR>')
-vim.keymap.set('i', 'jk', '<Esc>', { noremap = true, silent = true })
+vim.keymap.set('i', 'jk', '<Esc>', options)
 -- Move lines up
-vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { noremap = true, silent = true })
-vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { noremap = true, silent = true })
-vim.keymap.set("i", "<A-j>", "<Esc>:m .+1<CR>==gi", { noremap = true, silent = true })
-vim.keymap.set("i", "<A-k>", "<Esc>:m .-2<CR>==gi", { noremap = true, silent = true })
-vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
-vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", options)
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", options)
+vim.keymap.set("i", "<A-j>", "<Esc>:m .+1<CR>==gi", options)
+vim.keymap.set("i", "<A-k>", "<Esc>:m .-2<CR>==gi", options)
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", options)
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", options)
+-- resize window
+vim.keymap.set('n', '<A-Up>', ':resize -2<CR>', options)
+vim.keymap.set('n', '<A-Down>', ':resize +2<CR>', options)
+vim.keymap.set('n', '<A-Left>', ':vertical resize -2<CR>', options)
+vim.keymap.set('n', '<A-Right>', ':vertical resize +2<CR>', options)
